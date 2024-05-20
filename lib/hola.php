26eb00e5fe9373e48fe8575c0b6b5a78e581b624
Junglajaime <jaimerodriguezbenitez@iestorredelrey.es>
2024-05-20 21:06:04 +0200

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
 // Autor: Jaime <jaime@jaime.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
